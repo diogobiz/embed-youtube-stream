5df9df6756a76ebae93274559b8d7d1959e83d0a
Diogo Biz <diogo@codengage.com>
2019-11-19 12:50:56 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const styleWrapper = {
+  position: "relative",
+  paddingBottom: "56.25%",
+  paddingTop: 30,
+  height: 0,
+  overflow: "hidden"
+};
+
+const styleIframe = {
+  borderRadius: 4,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  border: 0
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        padding: 20,
+        display: "flex",
+        justifyContent: "space-between",
+        width: 1000,
+        margin: "0 auto"
+      }}
+    >
+      <div style={{ ...styleWrapper, width: "65%" }}>
+        <iframe
+          style={styleIframe}
+          src="https://www.youtube.com/embed/live_stream?channel=UCuKwxu1nZp3ysEixnMQpPjA"
+          allowFullScreen
+          title="Stream"
+        ></iframe>
+      </div>
+      <div style={{ ...styleWrapper, width: "33%" }}>
+        <iframe
+          style={styleIframe}
+          src="https://www.youtube.com/live_chat?v=2OUSGMAhSpg&embed_domain=localhost"
+          allowFullScreen
+          title="Chat"
+        ></iframe>
+      </div>
     </div>
   );
 }
