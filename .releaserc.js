7459684debea70485a1200b51d8fe62eb05fe4f5
diogobiz <diogo-biz@hotmail.com>
2020-03-03 22:23:11 -0300

    feat(core): set minial config to release action

--- .releaserc.js
+++ .releaserc.js
@@ -2,26 +2,13 @@ module.exports = {
   branch: "master",
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md"
-      }
-    ],
+    "@semantic-release/github",
     [
       "@semantic-release/npm",
       {
         npmPublish: false
       }
     ],
-    [
-      "@semantic-release/github",
-      {
-        assets: ["CHANGELOG.md", "package.json", "yarn-lock.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
+    "@semantic-release/release-notes-generator"
   ]
 };
